85f52020a64bc3351d634d5f699a7090cf819079
ramunas946 <ramunas946@gmail.com>
2020-07-05 17:44:13 +0300

    dajngo setup with heroku

--- pyshop/pyshop/settings.py
+++ pyshop/pyshop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,3 +125,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
