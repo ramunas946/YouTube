6d2294995d072e4032f4cd5d49f5a00d113c6309
ramunas946 <ramunas946@gmail.com>
2020-07-05 17:29:52 +0300

    init

---
+++ pyshop/products/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Product(models.Model):
+    image = models.CharField(max_length=2083)
+    videoTime = models.CharField(max_length=50)
+    icon = models.CharField(max_length=2083)
+    title = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    views = models.FloatField()
+    uploadeDay = models.FloatField()
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
